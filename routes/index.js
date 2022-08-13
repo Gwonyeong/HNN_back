15d2a6959b14c285bdb1d92aa963cf7f698eda77
Minsun91 <minsunkim167@gmail.com>
2022-08-13 08:22:19 +0200

    서버 연결 완료 추가

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 //인덱스 라우터에서 다른 라우터로 연결시켜줌.
-const postRouter = require("./sign.routes");
+const signRouter = require("./sign.routes"); //이름 겹쳐서 바꿔놨어요
+const postRouter = require("./post.routes");
 
-
-router.use("/sign", postRouter)
+router.use("/post", postRouter); //절단기능이다.
+router.use("/sign", signRouter);
 module.exports = router;
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 // const authMiddleware = require("../middlewares/auth-middleware");
 
-const PostsController = require("../controller/posts.controller");
+const PostsController = require("../controllers/posts.controller");
 const postsController = new PostsController();
 
 // 게시물 조회
