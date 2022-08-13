f05ea5026823579b3e7af4c6cf17f1f6c0c04072
gwonyeong <qwe980121@naver.com>
2022-08-13 15:27:17 +0900

    인덱스 파일 바꾸기

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 //인덱스 라우터에서 다른 라우터로 연결시켜줌.
-const postRouter = require("./sign.routes");
+const signRouter = require("./sign.routes");
 
 
-router.use("/sign", postRouter)
+router.use("/sign", signRouter)
 module.exports = router;
