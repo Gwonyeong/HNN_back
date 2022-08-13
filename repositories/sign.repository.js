570a7c0ac2beea5947f2a3fac91ea5aec675fd40
gwonyeong <qwe980121@naver.com>
2022-08-13 14:18:12 +0900

    회원가입 로그인 (이메일 닉네임)유효성 검사 , 로그아웃 기능

--- repositories/sign.repository.js
+++ repositories/sign.repository.js
@@ -1,8 +1,13 @@
 const { User } = require("../models");
 
 class UserRepository {
-    createUser = async (id, pw, nickname, MBTI) => {
-        const createUserData =await User.create({ id, pw, nickname, MBTI });
+    createUser = async (email, nickname, password, MBTI) => {
+        const createUserData = await User.create({
+            email,
+            password,
+            nickname,
+            MBTI,
+        });
         return createUserData;
     };
 
@@ -10,20 +15,21 @@ class UserRepository {
         const dupEmailData = await User.findOne({
             where: { email },
         });
+
         return dupEmailData;
     };
     checkDupNickname = async (nickname) => {
         const dupNicknameData = await User.findOne({
             where: { nickname },
         });
-        return dupNicknameData
-    }
-    signInUser = async (id, pw) => {
-        const loginUserData = await User.findOne({ where: { id, pw } });
-        
-        return loginUserData;
+        return dupNicknameData;
     };
-
+    findEmailToPassword = async (email) => {
+        const jwtPassword = await User.findOne({
+            where: { email },
+        });
+        return jwtPassword;
+    };
 }
 
 module.exports = UserRepository;
--- services/sign.service.js
+++ services/sign.service.js
@@ -1,36 +1,83 @@
-const UserRepository = require("../repositories/sign.repository");
+const SignRepository = require("../repositories/sign.repository");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const env = process.env;
 
 class SignService {
-    userRepository = new UserRepository();
+    signRepository = new SignRepository();
+    //로그인
+    //1. 정확한 이메일, 비밀번호를 입력시
+    login = async (email, password) => {
+        const userData = await this.signRepository.findEmailToPassword(email);
+        //email, password를 정상적으로 입력한 경우
+
+        if (!userData) {
+            return {
+                msg: "회원 정보가 일치하지 않습니다.",
+                success: false,
+                status: 400,
+            };
+        }
+        if (password === jwt.verify(userData.password, env.secretKey)) {
+            const token = jwt.sign(
+                {
+                    userId: userData.userId,
+                    nickname: userData.nickname,
+                    MBTI: userData.MBTI,
+                    profilePicture: userData.profilePicture,
+                },
+                env.secretKey
+            );
+            return {
+                token,
+                success: true,
+                status: 200,
+            };
+        }else return {
+            msg: "회원 정보가 일치하지 않습니다.",
+            success: false,
+            status: 400,
+        };
+    };
+
     //회원가입 : 아이디 중복버튼을 누른 상태여야 하고 비밀번호가
     //비밀번호 확인과 같아야 회원가입 완료.
     createUser = async (email, nickname, password, MBTI) => {
-        const createUserData =await this.userRepository.createUser(email, nickname, password, MBTI);
-        if(createUserData){
+        password = await jwt.sign(password, env.secretKey);
+        const createUserData = await this.signRepository.createUser(
+            email,
+            nickname,
+            password,
+            MBTI
+        );
+        if (createUserData.length !== 0) {
             return {
                 status: 200,
                 msg: "회원가입을 축하드립니다!",
                 success: true,
             };
-
         }
     };
-    
+
     //이메일 확인 : 이메일로 데이터베이스를 찾아 중복된 이메일이 있으면
     // true , 없으면 false
-    checkDupEmail = async(email) => {
-        const checkDupEmailData = this.userRepository.checkDupEmail(email)
-        if(checkDupEmailData){
-            return true
-        }return false
-    } 
+    checkDupEmail = async (email) => {
+        const checkDupEmailData = await this.signRepository.checkDupEmail(
+            email
+        );
 
-    checkDupNickname = async(nickname) => {
-        const checkDupNicknameData = this.userRepository.checkDupNickname(nickname)
-        if(checkDupNicknameData){
-            return true
-        }return false
-    } 
+        if (checkDupEmailData) {
+            return true;
+        } else return false;
+    };
+
+    checkDupNickname = async (nickname) => {
+        const checkDupNicknameData = await this.signRepository.checkDupNickname(
+            nickname
+        );
+        if (checkDupNicknameData) {
+            return true;
+        } else return false;
+    };
 }
 module.exports = SignService;
